9ee42ef2f767a2ed7d015778525bfed955c0e762
Sebastian Seitz <bassistsagainstracists@gmx.net>
2021-04-26 10:50:37 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+const cacheName = CACHE_NAME
+const urlsToCache = URLS_TO_CACHE
+/* eslint-enable no-undef */
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== cacheName) {
+            return caches.delete(name)
+          }
+
+          return null
+        })
+      )
+    })
+  )
+})
