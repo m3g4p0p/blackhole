3267f067552c9d01e89baf104bffd757afacbf13
Sebastian Seitz <bassistsagainstracists@gmx.net>
2021-04-26 10:53:39 +0200

    adjust deployment script

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 import json
 import re
+import shutil
 import subprocess
 
 
@@ -7,15 +8,21 @@ def dump(value):
     return json.dumps(value, indent=2).replace('"', "'")
 
 
-files = subprocess.run(['find', 'src', '-type', 'f'], capture_output=True)
+files = subprocess.run(
+    ['find', 'src', '-type', 'f', '-not', '-name', 'sw.js'],
+    capture_output=True)
+
 file_list = ['.'] + re.sub('^src', '.', files.stdout.decode(
     'utf-8').strip(), flags=re.MULTILINE).split('\n')
 
+shutil.rmtree('dist', ignore_errors=True)
+shutil.copytree('src', 'dist')
+
 with open('package.json') as package_json, \
-        open('sw-template.js') as template, \
-        open('src/sw.js', 'w') as sw:
+        open('src/sw.js') as sw_src, \
+        open('dist/sw.js', 'w') as sw_dist:
     version = json.load(package_json)['version']
-    sw.write(template.read().replace(
+    sw_dist.write(sw_src.read().replace(
         'CACHE_NAME',
         dump(version)
     ).replace(
